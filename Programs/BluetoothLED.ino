4f3adfb4ae657b296b4330951c596397f2723874
jsndc99 <40466093+jsndc99@users.noreply.github.com>
2018-08-28 22:49:20 +0530

    Add files via upload

--- Programs/BluetoothLED.ino
+++ Programs/BluetoothLED.ino
@@ -1,27 +1,33 @@
-char data = 0;                //Variable for storing received data
-void setup() 
+/*Bluetooth LED
+  This code turns internal LED 'ON' when signal is sent from a phone via bluetooth
 
+  LED_BUILTIN denotes the internal LED of any Arduino
+*/
+char data = 0;  //Variable for storing received data
+void setup()  //Mandatory function required in all 'sketches', runs only once after Arduino is switched ON 
 {
-  Serial.begin(9600);         //Sets the data rate in bits per second (baud) for serial data transmission
-  pinMode(LED_BUILTIN, OUTPUT);        //Sets built-in LED as output pin
+  Serial.begin(9600); //Sets the data rate in bits per second (baud rate) for serial data transmission
+  pinMode(LED_BUILTIN, OUTPUT); //Sets built-in LED as output pin
 }
 
-void loop()
+void loop()  //Mandatory function required in all 'sketches', runs forever till Arduino is switched OFF
 {
-  if(Serial.available() > 0)  // Send data only when you receive data:
+  if(Serial.available() > 0)  //Makes sure data from bluetoth TX can be received
   {
-    data = Serial.read();      //Read the incoming data and store it into variable data
-    Serial.print(data);        //Print Value inside data in Serial monitor
-    Serial.print("\n");        //New line 
-    if(data == '1')            //Checks whether value of data is equal to 1 
+    data = Serial.read(); //Read the incoming data and store it into variable data
+    Serial.print(data); //Print value inside data in Serial monitor
+    
+    Serial.print("\n"); //New line 
+    
+    if(data == '1') //Checks whether value of data is equal to 1 
     {
-      Serial.print("LED ON\n");
+      Serial.print("LED ON\n"); //Prints string in Serial Monitor
       digitalWrite(LED_BUILTIN, HIGH);  //If value is 1 then LED turns ON
     }
-    else if(data == '0')       //Checks whether value of data is equal to 0
+    else if(data == '0')  //Checks whether value of data is equal to 0
     {
-       Serial.print("LED OFF\n");
-      digitalWrite(LED_BUILTIN, LOW);   //If value is 0 then LED turns OFF
+       Serial.print("LED OFF\n"); //Prints string in Serial Monitor
+      digitalWrite(LED_BUILTIN, LOW); //If value is 0 then LED turns OFF
     }
   }                            
 }

15e52a6ca0d2abb5959c3b33d011d2511b75803c
jsndc99 <40466093+jsndc99@users.noreply.github.com>
2018-08-30 23:37:07 +0530

    Add files via upload

---
+++ Programs/Blink.ino
@@ -0,0 +1,19 @@
+/*Blink
+
+  Turns an LED on for one second, then off for one second, repeatedly.
+
+  LED_BUILTIN denotes the internal LED of any Arduino
+*/
+
+void setup()  //Mandatory function required in all 'sketches', runs only once after Arduino is switched ON 
+{
+  pinMode(LED_BUILTIN, OUTPUT); //Sets built-in LED as output pin
+}
+
+void loop() //Mandatory function required in all 'sketches', runs forever till Arduino is switched OFF
+{
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);  // wait for a second
+  digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
+  delay(1000);  // wait for a second
+}

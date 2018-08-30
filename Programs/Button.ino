ad57fd53aaf6c4fd6c54e3d8e3d2b742d2eaa5ad
jsndc99 <40466093+jsndc99@users.noreply.github.com>
2018-08-30 23:00:10 +0530

    Add files via upload

---
+++ Programs/Button.ino
@@ -0,0 +1,28 @@
+/*
+  Button
+  Turns on and off the internal LEDconnected to digital pin 13,
+  when pressing a pushbutton attached to pin 2
+
+  LED_BUILTIN denotes the internal LED of any Arduino
+*/
+const int buttonPin = 2;     // the number of the pushbutton pin
+
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup()   //Mandatory function required in all 'sketches', runs only once after Arduino is switched ON 
+{
+  pinMode(LED_BUILTIN, OUTPUT);  //Sets built-in LED as output pin
+  pinMode(buttonPin, INPUT);  //Sets button as input pin
+}
+
+void loop() //Mandatory function required in all 'sketches', runs forever till Arduino is switched OFF
+{
+  buttonState = digitalRead(buttonPin); // read the state of the pushbutton value:
+
+  if (buttonState == HIGH)   // check if the buttonstate is ON or OFF
+    digitalWrite(ledPin, HIGH); //If value is 1 then LED turns ON
+  else 
+    digitalWrite(ledPin, LOW);  //If value is 0 then LED turns OFF
+}
+
+

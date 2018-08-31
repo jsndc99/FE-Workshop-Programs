95f6afe0405f8c622bd46f89e1a5fd29c763469b
jsndc99 <40466093+jsndc99@users.noreply.github.com>
2018-08-31 22:33:51 +0530

    Update Bi-Dir_Communication.ino

--- Programs/Bi-Dir_Communication.ino
+++ Programs/Bi-Dir_Communication.ino
@@ -3,22 +3,37 @@
 
   SoftwareSerial is a header file consisting of functions making it easier to do a task
 */
-#include <SoftwareSerial.h> //informs program to use this file in the program
 
-SoftwareSerial mySerial (8,9);  //Creates an object named 'mySerial' such that pin 8 is used for receiving data and 9 for transimitting data
+//informs program to use this file in the program
+#include <SoftwareSerial.h> 
 
-void setup()  //Mandatory function required in all 'sketches', runs only once after Arduino is switched ON 
+//Creates an object named 'mySerial' such that pin 8 is used for receiving data and 9 for transimitting data
+SoftwareSerial mySerial (8,9);  
+
+//Mandatory function required in all 'sketches', runs only once after Arduino is switched ON
+void setup()   
 {
-  mySerial.begin(9600); //Sets the data rate in bits per second (baud rate) for serial data transmission through the phone
-  Serial.begin(9600); //Sets the data rate in bits per second (baud rate) for serial data transmission through the phone   
-  delay(100); // wait for serial port to connect, needed for native USB port only
+  //Sets the data rate in bits per second (baud rate) for serial data transmission through the phone
+  mySerial.begin(9600); 
+  
+  //Sets the data rate in bits per second (baud rate) for serial data transmission through the phone 
+  Serial.begin(9600);   
+  
+   // wait for serial port to connect, needed for native USB port only
+  delay(100);
 }
 
-void loop() //Mandatory function required in all 'sketches', runs forever till Arduino is switched OFF
-{
-  if (Serial.available()>0) //makes sure data from bluetoth TX can be received
-    mySerial.write(Serial.read());  //to send commands received from Serial Monitor(pc) to Mobile Phone
+//Mandatory function required in all 'sketches', runs forever till Arduino is switched OFF
+void loop() 
+{  
+  //makes sure data from bluetoth TX can be received  
+  if (Serial.available()>0) 
+  
+      //to send commands received from Serial Monitor(pc) to Mobile Phone
+      mySerial.write(Serial.read());  
 
-  if (mySerial.available()>0) //makes sure data from bluetoth TX can be received
-    Serial.write(mySerial.read());   //to send commands received from Mobile Phone to Serial Monitor(pc)
+  //makes sure data from bluetoth TX can be received
+  if (mySerial.available()>0) 
+    //to send commands received from Mobile Phone to Serial Monitor(pc)
+    Serial.write(mySerial.read());   
 }
